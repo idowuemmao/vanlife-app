8a40e7eb1a66567f54da0c290ca30eb572f37e73
Idowu Emmanuel <idowuemmao@gmail.com>
2023-08-25 15:02:57 +0100

    header responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header(){
         <nav className="flex w-full p-4 justify-between bg-slate-200 text-black font-medium">
             <NavLink to='/' style={(sty)=> sty.isActive ? myStyle : null } className="font-bold text-xl flex items-center ">
                 <img src={vanlogo} alt="van-logo" className="w-24 " />
-                <h2 className="text-4xl -ml-4">VANHIRE</h2>
+                <h2 className="text-4xl -ml-4 hidden sm:block">VANHIRE</h2>
             </NavLink>
             <div className="flex gap-4 items-center">
             <NavLink to="/host" style={({isActive})=> isActive ? myStyle : null } >Host</NavLink>
